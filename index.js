b728809b9aae558736a9684e95d34bfb75eb73dd
ckorir <chrisruto.k@gmail.com>
2023-09-16 13:44:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+function clickAlert() {
+    alert('I was clicked!');
+}
+button.addEventListener('click', clickAlert);
+
 function addingEventListener() {
+    button.addEventListener('click', clickAlert);
 }
